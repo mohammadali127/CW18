fd4d18d41e5bfcfd563262340b0bbbd16281d256
mohammadali127 <m.a.soltanhosseini@gmail.com>
2023-08-04 16:56:48 +0330

    chore: home.html fime was added.

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create, name='create'),
-    path('update/<int:todo_id>', views.update, name='update'),
 ]
